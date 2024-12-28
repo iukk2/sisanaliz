42fc731d85c6d0e7a8ba83c2a860a33f7663ba6a
iukk2 <159883369+iukk2@users.noreply.github.com>
2024-12-28 14:38:13 +0300

    Update task.py

--- task2/task.py
+++ task2/task.py
@@ -1,8 +1,8 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
-def main(path):
-    example = pd.read_csv(path, header = None)
+def main(var):
+    example = np.array(var)
     
     number_of_nodes = example.max().max()
     example_number_of_raws = example.shape[0]
@@ -15,7 +15,7 @@ def main(path):
     for i in range(1, number_of_nodes + 1):
         r_1 = 0
         for j in range(example_number_of_raws):
-            if i == example[0][j]:
+            if i == example[j][0]:
                 r_1 += 1
         final["r_1"][i] = r_1
     
@@ -24,7 +24,7 @@ def main(path):
     for i in range(1, number_of_nodes + 1):
         r_2 = 0
         for j in range(example_number_of_raws):
-            if i == example[1][j]:
+            if i == example[j][1]:
                 r_2 += 1
         final["r_2"][i] = r_2
         
@@ -33,8 +33,8 @@ def main(path):
     for i in range(1, number_of_nodes + 1):
         r_3 = 0
         for j in range(example_number_of_raws):
-            if i == example[0][j]:
-                r_3 += final["r_1"][example[1][j]]
+            if i == example[j][0]:
+                r_3 += final["r_1"][example[j][1]]
         final["r_3"][i] = r_3
         
     #r_4: отношение опосредственного подчинения
@@ -42,8 +42,8 @@ def main(path):
     for i in range(1, number_of_nodes + 1):
         r_4 = 0
         for j in range(example_number_of_raws):
-            if i == example[1][j]:
-                r_4 += final["r_2"][example[0][j]]
+            if i == example[j][1]:
+                r_4 += final["r_2"][example[j][0]]
         final["r_4"][i] = r_4
         
     #r_5: отношение соподчинения на одном уровне
@@ -52,13 +52,15 @@ def main(path):
         # Для начала найдем узел которому подчиняется i.
         boss = 0
         for j in range(example_number_of_raws):
-            if i == example[1][j]:
-                boss = example[0][j]
+            if i == example[j][1]:
+                boss = example[j][0]
         if boss == 0:
             final["r_5"][i] = 0
         else:
             final["r_5"][i] = final["r_1"][boss] - 1
-            
+    
     final = np.array(final)
     return final
 
+var = [[1,2],[1,3],[3,4],[3,5]]
+print(main(var))
